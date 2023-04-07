e4773816704c00f662b0499c376db9664cf5d072
Rose Soriano <rosesoriano.dtx@gmail.com>
2023-04-06 23:34:22 -0700

    Use tailwind css typography

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
-<style>
-
-</style>
-
-<main>
+<div class="prose container mx-auto rounded-xl">
     <h1>Rose Soriano</h1>
     <p>
         <a href="https://github.com/rosesori">github.com/rosesori</a>
     </p>
-</main>
+</div>
+
+<div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p class="text-3xl text-gray-700 font-bold mb-5">
+       Welcome!
+    </p>
+    <p class="text-gray-500 text-lg">
+       SvelteKit and Tailwind CSS in action
+    </p>
+</div>
